a293c7d6edb3bc4ceaa0e2cbe93c2c1c1a4a3cd4
iapposki <konark981999@gmail.com>
2023-10-08 15:32:53 +0530

    added logic for gender, saving gender while assigning relationship, and minor bugs corrected

--- logic.py
+++ logic.py
@@ -2,13 +2,19 @@ import json
 
 
 class Member: 
-    def __init__(self, name, gender = None) :
-        self.name = name
-        self.gender = gender
+    def __init__(self, info) :
+        parts = info.split(" ")
+        if parts[-1] == 'female' or parts[-1] == 'male':
+            self.name = " ".join(parts[0:len(parts)-1])
+            self.gender = parts[-1]
+        else:
+            self.name = " ".join(parts)
+            self.gender = None
+            
         self.relation = {}
         new_member = {'name': self.name, 'gender': self.gender, 'relation': self.relation}
         data = self.load_data()
-        if self.name in data['members']:
+        if self.name.lower() in data['members']:
             res = 'Sorry...member with same name already present'
         else:
             with open('db.json', 'w') as file:
@@ -35,15 +41,56 @@ class Member:
 
     @staticmethod
     def connect(sentence):
+        relationship_dict = {
+            "father": "son daughter",
+            "mother": "son daughter",
+            "brother": "brother sister",
+            "sister": "brother sister",
+            "grandmother": "grandson granddaughter",
+            "grandfather": "grandson granddaughter",
+            "grandson": "grandfather grandmother",
+            "granddaughter": "grandfather grandmother",
+            "uncle": "nephew niece",
+            "aunt": "nephew niece",
+            "nephew": "uncle aunt",
+            "niece": "uncle aunt",
+            "son": "father mother",
+            "daughter": "father mother",
+            "husband": "husband wife",
+            "wife": "husband wife",
+        }
+        female_relationship_set = {'sister', 'mother', 'grandmother', 'aunt', 'niece', 'daughter', 'wife'}
         parts = sentence.split(" ")
         first_name = " ".join(parts[0:parts.index('as')])
         second_name = " ".join(parts[parts.index('of')+ 1:])
-        relation = parts[parts.index('as')+1]
+        relation = parts[parts.index('as')+1].lower()
         data = Member.load_data()
         if relation in data['members'][second_name.lower()]['relation']:
             data['members'][second_name.lower()]['relation'][relation].append(first_name.lower())
+            data['members'][second_name.lower()]['relation'][relation] = list(set(data['members'][second_name.lower()]['relation'][relation]))
+            if relation in female_relationship_set:
+                data['members'][first_name.lower()]['gender'] = 'female'
+            else:
+                data['members'][first_name.lower()]['gender'] = 'male'
         else: 
             data['members'][second_name.lower()]['relation'][relation] = [first_name.lower()]
+            if relation in female_relationship_set:
+                data['members'][first_name.lower()]['gender'] = 'female'
+            else:
+                data['members'][first_name.lower()]['gender'] = 'male'
+
+        if data['members'][second_name.lower()]['gender'] == 'female':
+            if relationship_dict[relation].split(" ")[1] in data['members'][first_name.lower()]['relation']:
+                data['members'][first_name.lower()]['relation'][relationship_dict[relation].split(" ")[1]].append(second_name.lower())
+                data['members'][first_name.lower()]['relation'][relationship_dict[relation].split(" ")[1]] = list(set(data['members'][first_name.lower()]['relation'][relationship_dict[relation].split(" ")[1]]))
+            else:
+                data['members'][first_name.lower()]['relation'][relationship_dict[relation].split(" ")[1]] = [second_name.lower()]
+        elif data['members'][second_name.lower()]['gender'] == 'male' :
+            if relationship_dict[relation].split(" ")[0] in data['members'][first_name.lower()]['relation']:
+                data['members'][first_name.lower()]['relation'][relationship_dict[relation].split(" ")[0]].append(second_name.lower())
+                data['members'][first_name.lower()]['relation'][relationship_dict[relation].split(" ")[0]] = list(set(data['members'][first_name.lower()]['relation'][relationship_dict[relation].split(" ")[0]]))
+            else:
+                data['members'][first_name.lower()]['relation'][relationship_dict[relation].split(" ")[0]] = [second_name.lower()]
         Member.dump_data(data)
         # print(first_name, second_name, relation)
 
